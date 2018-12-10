9302e266c6a6fbe9fe083acc322b260b976b6aa9
JeremySvn <svensson.jeremy94@gmail.com>
2018-12-10 17:19:44 +0100

    Command Line OK

---
+++ functions.h
@@ -0,0 +1,100 @@
+typedef struct lineLevels lineLevels;
+struct lineLevels{
+    int identifier;
+    int levelNumber;
+
+    int startLevel;
+    int startingLine;
+    int endLevel;
+    int endingLine;
+    char*** declaredVars;           // int, float, char, double, long, struct
+    int* nbVars;
+    lineLevels* sonStructs;
+    int nbSons;
+
+    char*** globalVars;
+    int* nbGlobal;
+
+    int dadAddr;
+};
+
+int testDisplay = 0;
+
+// Recuperation contenu d'un fichier
+char* getFileContent(char* path);
+
+// Récupération des valeurs des règles dans les fichiers de configuration
+int* getConfRules(char* fileContent);
+int* agregateRulesValues(int nbExtend, char** fileNames);
+
+// Récupération des héritages d'un fichier de configuration
+int getNbFilesExtended(char* fileContent);
+char** getConfExtends(char* fileContent);
+
+// Récupération de la chaine de nom de fichiers de configuration (récursif)
+int getNbExtendLayer(char* fileContent);
+char** getAllExtendedFiles(char* currFileName, char* fileContent, int nbExtends);
+
+// Récupération des fichiers exclus du traitement par les fichiers de configuration
+int getNbFilesExcluded(char* fileContent);
+int agregateNbFilesExcluded(int nbExtend, char** fileNames);
+char** getConfExcluded(char* fileContent);
+char** agregateConfExcluded(int nbExtend, char** fileNames, int nbFilesExcluded);
+
+// Récupération de la valeur du critère récursif dans les fichiers de configuration
+int isRecursive(char* fileContent);
+int agregateIsRecursive(int nbExtend, char** fileNames);
+
+void dispDirContent(char* path, int searchType, int nbFilesExcluded, char** excludedFiles, int typeExec, int* rulesValues);
+
+// Fonctions relatives au parsing d'un fichier de code source
+int getNbPrimaryLevels(char* fileContent);
+lineLevels* getAllLevels(char* fileContent, int nbPrimaries);
+
+int getNbInsiderLevels(char* fileContent, int startInd, int endInd);
+lineLevels* getInsidersLevels(char* fileContent, int nbPrimaries, lineLevels motherStruct);
+
+int* levelVarCount(char* fileContent, lineLevels currStruct);
+int countVarsOnLine(char* fileContent, int startInd);
+
+int getLineOfVar(char* varName);
+char* extractNameFromNameVar(char* varName);
+
+int* getNbGlobalVars(char* fileContent, lineLevels* primaryStructs, int nbPrimaries);
+char*** getGlobalVars(char* fileContent, lineLevels* primaryStructs, int nbPrimaries, int* nbVars);
+
+char*** levelVarStorage(char* fileContent, lineLevels currStruct);
+char** extractVarsOfLine(char* fileContent, int startInd, int nbVarsOnLine, int lineNumber);
+
+lineLevels assignGlobalTabsInSons(lineLevels currStruct, char*** globalVars, int* nbGlobal);
+
+void freeAllStructs(lineLevels* primaryStructs, int nbStructs);
+
+// Fonctions simple de traitement spécifique
+// (isText = vérif code ASCII d'un char)
+// dispErrMessg + isExistingFile = vérif d'existence (et de présence de contenu) et affichage d'erreur en fonction
+int isText(char c);
+int isExistingFile(char* path);
+int dispErrMessg(char* fileName, int typeOf, int dispChx);
+
+// Fonctions relatives aux vérifications des règles
+void verifSourceCode(char* path, int* rulesValues);
+int lookForType(char* fileContent, int startInd);
+
+int* getMultiDeclarOnLine(char* fileContent, lineLevels* primaryStructs, int nbPrimaryLevels, int nbLines);
+int* structNbVarsOnLines(lineLevels currStruct, int* tabOfLines);
+
+int* getVarsDeclaredButUnused(char* fileContent, int nbLines, lineLevels* primaryStructs, int nbPrimaries);
+int checkIfVarUsedInStructAndSons(char* fileContent, char* nameVar, lineLevels* primaryStructs, int nbPrimaries, int isGlobal, int levelNumber, int identifier);
+
+// Fonctions de reperage de deuxieme declaration d'une variable
+int* getLinesOfAlreadyDeclaredVars(char* fileContent, int nbLines, lineLevels* primaryStructs, int nbPrimaries);
+int* goToStructSonsForCheck(lineLevels* primaryStructs, int nbPrimaries, lineLevels currStruct, int* tabOfLines, int chxExec, char* fileContent);
+
+int checkIfGlobalExistInGlobal(char*** globalVars, int* nbGlobal, int typeIndex, int varIndex);
+int checkIfExistInStruct(lineLevels* primaryStructs, int nbPrimaries, lineLevels currStruct, int typeIndex, int varIndex);
+
+lineLevels getStructWithLevelAndIdentifier(int targetLevel, int targetIdentifier, lineLevels* primaryStructs, int nbPrimaries);
+lineLevels getSonStructWithLevelAndIdentifier(int targetLevel, int targetIdentifier, lineLevels currStruct, int* checkPointer);
+
+int varAlreadyDeclaredInScope(lineLevels* primaryStructs, int nbPrimaries, char* nameVar);
--- main.c
+++ main.c
@@ -2,108 +2,8 @@
 #include <stdlib.h>             //!     Etudiants :
 #include <dirent.h>             //!         Svensson Jeremy
 #include <string.h>             //!         Remy
-                                //!         Michael
+#include "functions.h"          //!         Michael
 
-typedef struct lineLevels lineLevels;
-struct lineLevels{
-    int identifier;
-    int levelNumber;
-
-    int startLevel;
-    int startingLine;
-    int endLevel;
-    int endingLine;
-    char*** declaredVars;           // int, float, char, double, long, struct
-    int* nbVars;
-    lineLevels* sonStructs;
-    int nbSons;
-
-    char*** globalVars;
-    int* nbGlobal;
-
-    int dadAddr;
-};
-
-int testDisplay = 0;
-
-// Recuperation contenu d'un fichier
-char* getFileContent(char* path);
-
-// Récupération des valeurs des règles dans les fichiers de configuration
-int* getConfRules(char* fileContent);
-int* agregateRulesValues(int nbExtend, char** fileNames);
-
-// Récupération des héritages d'un fichier de configuration
-int getNbFilesExtended(char* fileContent);
-char** getConfExtends(char* fileContent);
-
-// Récupération de la chaine de nom de fichiers de configuration (récursif)
-int getNbExtendLayer(char* fileContent);
-char** getAllExtendedFiles(char* currFileName, char* fileContent, int nbExtends);
-
-// Récupération des fichiers exclus du traitement par les fichiers de configuration
-int getNbFilesExcluded(char* fileContent);
-int agregateNbFilesExcluded(int nbExtend, char** fileNames);
-char** getConfExcluded(char* fileContent);
-char** agregateConfExcluded(int nbExtend, char** fileNames, int nbFilesExcluded);
-
-// Récupération de la valeur du critère récursif dans les fichiers de configuration
-int isRecursive(char* fileContent);
-int agregateIsRecursive(int nbExtend, char** fileNames);
-
-void dispDirContent(char* path, int searchType, int nbFilesExcluded, char** excludedFiles, int typeExec, int* rulesValues);
-
-// Fonctions relatives au parsing d'un fichier de code source
-int getNbPrimaryLevels(char* fileContent);
-lineLevels* getAllLevels(char* fileContent, int nbPrimaries);
-
-int getNbInsiderLevels(char* fileContent, int startInd, int endInd);
-lineLevels* getInsidersLevels(char* fileContent, int nbPrimaries, lineLevels motherStruct);
-
-int* levelVarCount(char* fileContent, lineLevels currStruct);
-int countVarsOnLine(char* fileContent, int startInd);
-
-int getLineOfVar(char* varName);
-char* extractNameFromNameVar(char* varName);
-
-int* getNbGlobalVars(char* fileContent, lineLevels* primaryStructs, int nbPrimaries);
-char*** getGlobalVars(char* fileContent, lineLevels* primaryStructs, int nbPrimaries, int* nbVars);
-
-char*** levelVarStorage(char* fileContent, lineLevels currStruct);
-char** extractVarsOfLine(char* fileContent, int startInd, int nbVarsOnLine, int lineNumber);
-
-lineLevels assignGlobalTabsInSons(lineLevels currStruct, char*** globalVars, int* nbGlobal);
-
-void freeAllStructs(lineLevels* primaryStructs, int nbStructs);
-
-// Fonctions simple de traitement spécifique
-// (isText = vérif code ASCII d'un char)
-// dispErrMessg + isExistingFile = vérif d'existence (et de présence de contenu) et affichage d'erreur en fonction
-int isText(char c);
-int isExistingFile(char* path);
-int dispErrMessg(char* fileName, int typeOf, int dispChx);
-
-// Fonctions relatives aux vérifications des règles
-void verifSourceCode(char* path, int* rulesValues);
-int lookForType(char* fileContent, int startInd);
-
-int* getMultiDeclarOnLine(char* fileContent, lineLevels* primaryStructs, int nbPrimaryLevels, int nbLines);
-int* structNbVarsOnLines(lineLevels currStruct, int* tabOfLines);
-
-int* getVarsDeclaredButUnused(char* fileContent, int nbLines, lineLevels* primaryStructs, int nbPrimaries);
-int checkIfVarUsedInStructAndSons(char* fileContent, char* nameVar, lineLevels* primaryStructs, int nbPrimaries, int isGlobal, int levelNumber, int identifier);
-
-// Fonctions de reperage de deuxieme declaration d'une variable
-int* getLinesOfAlreadyDeclaredVars(char* fileContent, int nbLines, lineLevels* primaryStructs, int nbPrimaries);
-int* goToStructSonsForCheck(lineLevels* primaryStructs, int nbPrimaries, lineLevels currStruct, int* tabOfLines, int chxExec, char* fileContent);
-
-int checkIfGlobalExistInGlobal(char*** globalVars, int* nbGlobal, int typeIndex, int varIndex);
-int checkIfExistInStruct(lineLevels* primaryStructs, int nbPrimaries, lineLevels currStruct, int typeIndex, int varIndex);
-
-lineLevels getStructWithLevelAndIdentifier(int targetLevel, int targetIdentifier, lineLevels* primaryStructs, int nbPrimaries);
-lineLevels getSonStructWithLevelAndIdentifier(int targetLevel, int targetIdentifier, lineLevels currStruct, int* checkPointer);
-
-int varAlreadyDeclaredInScope(lineLevels* primaryStructs, int nbPrimaries, char* nameVar);
 
 /*
     LES REGLES
@@ -219,9 +119,40 @@ int* getAffectWithWrongType(){
 
 int main(int argc, char **argv)
 {
+    char initConf[128];
+    char path[128];
+
+    if(argc != 3){
+        printf("Pas assez de paramètres\n");
+        printf("L'appel du programme doit etre :\n./nomProg dossierConfig dossierCible\n\n\n\n");
+        strcpy(initConf, "conf");
+        strcpy(path, ".");
+
+        printf("Voulez vous executer le programme selon ses parametres par defaut ?\n1 - Oui\n2 - Non\n");
+        int userChx = 0;
+        while(userChx < 1 || userChx > 2){
+            printf(">> ");
+            scanf("%d", &userChx);
+        }
+
+        if(userChx == 1){
+            system("cls");
+            printf("Lancement du programme selon la configuration par defaut...\n\n");
+            system("pause");
+        }else{
+            system("cls");
+            printf("Veuillez relancer le programme avec les bons parametres.\n\n");
+            system("pause");
+            return 0;
+        }
+    }else{
+        strcpy(initConf, argv[1]);
+        strcpy(path, argv[2]);
+    }
+
     char rulesName[17][50] = {"array-bracket-eol","operators-spacing","comma-spacing","indent","comments-header","max-line-numbers","max-file-line-numbers","no-trailing-spaces","no-multi-declaration","unused-variable","undeclared-variable","no-prototype","unused-function","undeclared-function","variable-assignment-type","function-parameters-type","variable-no-double-declaration"};
 
-    char initConf[128] = "conf/main.lconf";
+    strcat(initConf, "/main.lconf");
     int i;
 
     // Tableau contenant la valeur de chaque règle
@@ -354,21 +285,22 @@ int main(int argc, char **argv)
             printf("1 - Execution du Linter selon sa configuration\n");
             printf("2 - Lister les fichiers cibler par le Linter\n");
             printf("3 - Execution du Linter sur un fichier test : 'exemple.c'\n");
-            printf("0 - QUITTER\n>>");
+            printf("0 - QUITTER\n");
             userChx = -1;
             while(userChx < 0 || userChx > 3){
+                printf(">> ");
                 scanf("%d",&userChx);
             }
 
             switch(userChx){
             case 1:
                 system("cls");
-                dispDirContent(".", recursiveState, nbFileExcluded, excludedFiles, 1, rulesValues);
+                dispDirContent(path, recursiveState, nbFileExcluded, excludedFiles, 1, rulesValues);
                 break;
 
             case 2:
                 system("cls");
-                dispDirContent(".", recursiveState, nbFileExcluded, excludedFiles, 0, rulesValues);
+                dispDirContent(path, recursiveState, nbFileExcluded, excludedFiles, 0, rulesValues);
                 system("pause");
                 break;
 
